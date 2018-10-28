032fe41046050f92c8b3ac061089549b6590e1ae
Robert Vidal <bobbyvidal@gmail.com>
2018-10-28 02:04:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 function shout(string) {
   return 'hello'.toUpperCase();
 }
+
+function whisper(string) {
+  return 'HELLO'.toLowerCase();
+}
+
+function logShout(string) {
+  console.log('hello'.toUpperCase());
+}
